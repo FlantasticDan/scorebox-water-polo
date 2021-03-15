15298af7cf880d03fe6b5363e7261550d35d067d
Daniel Flanagan <dancharley8@gmail.com>
2021-03-14 22:19:03 -0700

    Frontend Setup UI

--- flask/main.py
+++ flask/main.py
@@ -1,12 +1,40 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, send_file
 from flask_socketio import SocketIO
 
 from consoles.sports import WaterPoloDaktronics
 
+from images import Logos
+
 VERSION = 'v0.0.1 (03142021)'
+LOGOS = Logos()
 
 app = Flask(__name__)
 socketio = SocketIO(app, cors_allowed_origins="*", async_mode='eventlet')
+
+@app.route('/setup')
+def setup():
+    return render_template('setup.html', version=VERSION)
+
+@app.route('/init', methods=['POST'])
+def initialize():
+    global LOGOS
+    setup = request.form
+    files = request.files
+    LOGOS.set_home(files['home_logo'].read(), files['home_logo'].filename)
+    LOGOS.set_visitor(files['visitor_logo'].read(), files['visitor_logo'].filename)
+    return 'OK'
+
+@app.route('/home')
+def home():
+    global LOGOS
+    output, mimetype = LOGOS.home()
+    return send_file(output, mimetype=mimetype, as_attachment=False)
+
+@app.route('/visitor')
+def visitor():
+    global LOGOS
+    output, mimetype = LOGOS.visitor()
+    return send_file(output, mimetype=mimetype, as_attachment=False)
 
 @app.route('/overlay')
 def overlay():
